dbe37ec8968f1c4cb7ea8b1f484bafa4b79a83dc
shamiul <anik.savar.bd@gmail.com>
2023-03-10 23:09:00 +0600

    Q and A Section Updated

---
+++ assets/js/scripts.js
@@ -0,0 +1,55 @@
+(function ($) {
+  $.fn.textAnimation = function (animation_speed, text_speed, animation) {
+    var text,
+      i = 0;
+    var $this = $(this);
+    console.log("this ", $this); // #SI
+
+    // store text and clear
+    text = $this.text();
+    console.log("text: ", text); // #SI
+    // $this.css('white-space', 'pre');
+    $this.html("");
+    console.log("this ", $this.html("")); // #SI
+
+    var addLetter = setInterval(function () {
+      $this.append(
+        '<div class="text_animation" style="display: inline-block; animation: ' +
+          animation +
+          " " +
+          animation_speed +
+          'ms forwards">' +
+          text[i] +
+          "</div>"
+      );
+      i++;
+      // console.log("i++: ", i); // #SI
+      if (i == text.length) clearInterval(addLetter);
+    }, text_speed);
+  };
+})(jQuery);
+
+// var temp = $('.bounce_in_animation').text();
+// console.log("temp ", temp); // #SI
+// var i = 1;
+
+$(".bounce_in_animation").textAnimation(250, 50, "bounceIn");
+
+// setInterval(function () {
+//   i %= 4;
+//   $('.bounce_in_animation').html(temp);
+//   switch (i) {
+//     case 0:
+//       $('.bounce_in_animation').textAnimation(250, 50, 'bounceIn');
+//       break;
+//     case 1:
+//       $('.bounce_in_animation').textAnimation(250, 50, 'slideDown');
+//       break;
+//     case 2:
+//       $('.bounce_in_animation').textAnimation(250, 50, 'slideUp');
+//       break;
+//     default:
+//       $('.bounce_in_animation').textAnimation(250, 50, 'spinIn');
+//   }
+//   i++;
+// }, 1000 + 50 * temp.length + 250)
